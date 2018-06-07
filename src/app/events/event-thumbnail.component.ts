2fdfec230ca451cd6a2e4d00cb3a74e23f8882cb
FoxKat <kfox3151@gmail.com>
2018-06-07 09:01:02 +0100

    Adding variable to a child component to call it's functions from the parent

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -20,8 +20,13 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 export class EventThumbnailComponent {
    @Input() event: any;
    @Output() eventClick = new EventEmitter();
+   someProperty: any = 'some value';
 
    handleClickMe() {
        this.eventClick.emit(this.event.name);
+   }
+
+   logFoo() {
+       console.log('foo');
    }
 }
