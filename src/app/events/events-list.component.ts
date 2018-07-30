7a967c5a05facf88086bc8f22f5da86dc9183b16
FoxKat <kfox3151@gmail.com>
2018-07-30 13:43:36 +0100

    dsadsad

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -2,17 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-events-list',
-    template: `
-    <div>
-        <h1>Upcoming Angular Events</h1>
-        <hr/>
-        <div class="row">
-            <div *ngFor="let event of events" class="col-md-5">
-                <event-thumbnail [event]="event"></event-thumbnail>
-            </div>
-        </div>
-    </div>
-    `
+    templateUrl: 'events-list.component.html'
 })
 
 export class EventListComponent {
