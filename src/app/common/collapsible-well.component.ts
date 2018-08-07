149d7cd75faf516a1dff3fea4ebdf884fe8f8e08
FoxKat <kfox3151@gmail.com>
2018-08-07 10:08:32 +0100

    Multiple slot content projection (the little flame)

--- src/app/common/collapsible-well.component.ts
+++ src/app/common/collapsible-well.component.ts
@@ -1,11 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'collapsible-well',
     templateUrl: './collapsible-well.component.html'
 })
 export class CollapsibleWellComponent {
-    @Input() title: string;
     visible: boolean = true;
 
     toggleContent() {
