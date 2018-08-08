3efced9402d5bb64f5bb8934e64405a79dfd929f
FoxKat <kfox3151@gmail.com>
2018-08-08 09:28:48 +0100

    Sorting Data

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -14,7 +14,8 @@ export class EventDetailsComponent implements OnInit {
     event: IEvent;
     addMode: Boolean;
     filterBy: string = 'all';
-    
+    sortBy: string = 'votes';
+
     constructor(private eventService: EventService, private route: ActivatedRoute) {
     }
     ngOnInit() {
